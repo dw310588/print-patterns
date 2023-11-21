3019d144991766753bb5d0908aec429b5ea10d91
Daniel Wilk <daniel.3222a@gmail.com>
2023-11-21 16:46:43 +0100

    Quick fix: pattern 1

--- pattern1.cpp
+++ pattern1.cpp
@@ -8,8 +8,8 @@ int main() {
 
   std::cout << "---------------" << std::endl;
 
-  for(int i = 1; i <= n; i++) {
-    for(int j = 1; j <= i; j++) {
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j <= i; j++) {
       std::cout << "*";
     }
     std::cout << std::endl;
